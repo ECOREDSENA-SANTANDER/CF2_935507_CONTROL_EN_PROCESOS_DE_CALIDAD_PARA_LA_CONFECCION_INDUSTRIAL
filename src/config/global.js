57b192120ad76a84f3b93107ca4df62c4f1295e2
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-08-12 16:37:22 -0500

    avance

--- src/config/global.js
+++ src/config/global.js
@@ -264,7 +264,7 @@ export default {
             'Centro para la Industria de la Comunicación Gráfica - Regional Distrito Capital',
         },
         {
-          nombre: 'JJulieth Paola Vital López',
+          nombre: 'Julieth Paola Vital López',
           cargo: 'Corrección de estilo',
           centro:
             'Centro para la Industria de la Comunicación Gráfica - Regional Distrito Capital',
